ecd98ab86fa37380cd017f743e396877129c8a0a
Leka <lmartashvili1@gmail.com>
2026-04-22 16:58:50 +0400

    feat: add auth service and auth guard

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,10 +1,10 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../services/auth';
 
 export const authGuard: CanActivateFn = () => {
-  const auth = inject (AuthService);
-  const router = inject (Router);
+  const auth = inject(AuthService);
+  const router = inject(Router);
 
   if (auth.isLoggedIn()) {
     return true;
--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,6 +1,59 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Auth {}
+export interface SignInRequest {
+  email: string;
+  password: string;
+}
+
+export interface SignUpRequest {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  avatar: string;
+}
+
+export interface AuthResponse {
+  access_token: string;
+  refresh_token: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private http = inject(HttpClient);
+  private router = inject(Router);
+  private apiUrl = 'https://api.everrest.educata.dev';
+
+  isLoggedIn = signal<boolean>(!!localStorage.getItem('access.token'));
+
+  signIn(body: SignInRequest) {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/sign_in`, body).pipe(
+      tap(res => {
+        localStorage.setItem('access.token', res.access_token);
+        localStorage.setItem('refresh.token', res.refresh_token);
+        this.isLoggedIn.set(true);
+      })
+    );
+  }
+
+  signUp(body: SignUpRequest) {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/sign_up`, body).pipe(
+      tap(res => {
+        localStorage.setItem('access.token', res.access_token);
+        localStorage.setItem('refresh.token', res.refresh_token);
+        this.isLoggedIn.set(true);
+      })
+    );
+  }
+
+  signOut() {
+    localStorage.removeItem('access.token');
+    localStorage.removeItem('refresh.token');
+    this.isLoggedIn.set(false);
+    this.router.navigate(['/login']);
+  }
+}
