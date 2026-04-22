44ff7508cd4488451fdd7d630274f80e7eff86aa
Leka <lmartashvili1@gmail.com>
2026-04-22 15:50:08 +0400

    init commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { tokenInterceptor } from './core/interceptors/token.interceptor';
+import { tokenInterceptor } from './core/interceptors/token-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(withInterceptors([tokenInterceptor]))],
