b7868636f89bbdc4ee0e3bb90012a0a3e6bb7f62
Leka <lmartashvili1@gmail.com>
2026-04-22 15:41:17 +0400

    app.config.ts & token-interceptor.ts done

--- src/app/core/interceptors/token-interceptor.ts
+++ src/app/core/interceptors/token-interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('access_token');
+
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(cloned);
+  }
   return next(req);
 };
