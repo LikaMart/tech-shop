c4042b201a44b883dc379e8ab6e1af8efb3a5595
Leka <lmartashvili1@gmail.com>
2026-04-24 15:35:05 +0400

    proxy if  API isn't work well

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  error = signal<string>('');
+  isLoading = signal<boolean>(false);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  submit() {
+    if (this.form.invalid) return;
+    this.isLoading.set(true);
+    this.error.set('');
+
+    this.auth.signIn(this.form.value as any).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: () => {
+        this.error.set('არასწორი მეილი ან პაროლი');
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
