0cea67d268867abc125154eb5d6a9ab9a826dc7c
Leka <lmartashvili1@gmail.com>
2026-05-03 15:45:45 +0400

    add toSignal, error handling and loading state

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,35 +1,35 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ProductService, Product } from '../../core/services/product';
 import { CartService } from '../../core/services/cart';
 import { RouterLink } from '@angular/router';
 import { GelPipe } from '../../shared/pipes/gel-pipe';
+import { signal } from '@angular/core';
+import { map, catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink, GelPipe],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
-  products = signal<Product[]>([]);
-  isLoading = signal<boolean>(false);
+  error = signal<string>('');
 
-  ngOnInit() {
-    this.isLoading.set(true);
-    this.productService.getAll().subscribe({
-      next: (res) => {
-        this.products.set(res.products);
-        this.isLoading.set(false);
-      },
-      error: () => {
-        this.isLoading.set(false);
-      },
-    });
-  }
+  products = toSignal(
+    this.productService.getAll().pipe(
+      map(res => res.products),
+      catchError(() => {
+        this.error.set('პროდუქტები ვერ ჩაიტვირთა');
+        return of([]);
+      })
+    ),
+    { initialValue: [] as Product[] }
+  );
 
   addToCart(product: Product) {
     if (this.cartService.cart()) {
