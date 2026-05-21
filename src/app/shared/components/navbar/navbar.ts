016d5b6122d4b3a15bcf4675bba7d87809fe1852
Leka <lmartashvili1@gmail.com>
2026-05-22 00:07:24 +0400

    fix burger menu

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, Router, NavigationStart } from '@angular/router';
 import { AuthService } from '../../../core/services/auth';
 import { CartService } from '../../../core/services/cart';
 import { LanguageService } from '../../../core/services/language';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -15,7 +16,15 @@ export class NavbarComponent {
   auth = inject(AuthService);
   cart = inject(CartService);
   lang = inject(LanguageService);
+  private router = inject(Router);
+
   isMenuOpen = signal(false);
+
+  constructor() {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationStart)
+    ).subscribe(() => this.closeMenu());
+  }
 
   toggleMenu() {
     this.isMenuOpen.update((v) => !v);
